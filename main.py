cc8a7dca682ed28b55760324ee4053d84bed71a5
sasiis80-gif <sasini.somaratne80@gmail.com>
2026-03-10 19:29:28 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+#Storing values
+tree1 = 98
+tree2 = 94
+tree3 = 41
+tree4 = 95
+tree5 = 11
+
+#Finding the total of trees
+sum = tree1+tree2+tree3+tree4+tree5
+print("the sum of all the 5 trees are:", sum)
+
+#Finding the avarage of trees
+average = sum/5
+print("the avarage of all the trees are:", average)
